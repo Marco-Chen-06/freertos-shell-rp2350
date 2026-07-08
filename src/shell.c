55647cc959f1f3f60f229c09e276903b6270fbe8
Marco-Chen-06 <marcochen.work@gmail.com>
2026-07-08 10:58:25 -0400

    add UART0 peri to handle picocom input

--- src/shell.c
+++ src/shell.c
@@ -5,7 +5,27 @@ static uint8_t inputBuf[SHELL_INPUT_BUF_LEN];
 static uint8_t *inputBufPtr;
 static uint8_t outputBuf[configCOMMAND_INT_MAX_OUTPUT_SIZE];
 
+void uart_rx_handler() {
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    while (uart_is_readable(UART_PERI)) {
+        uint8_t ch = uart_getc(UART_PERI);
+        xQueueSendFromISR(xRecvQueue, &ch, &xHigherPriorityTaskWoken);
+    }
+    portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
+}
+
 void shell_init() {
+    uart_init(UART_PERI, UART_BAUDRATE);
+
+    // set GPIO0 and GPIO1 to UART TX and RX respectively
+    gpio_set_function(0, GPIO_FUNC_UART);
+    gpio_set_function(1, GPIO_FUNC_UART);
+
+    irq_set_exclusive_handler(UART_IRQ, uart_rx_handler);
+    irq_set_enabled(UART_IRQ, true);
+    uart_set_irq_enables(UART_PERI, true, false); // UARTRXINT on, UARTRXINT off
+
+
     inputBufPtr = inputBuf;
     xRecvQueue = xQueueCreate(SHELL_RECV_QUEUE_LEN, sizeof(uint8_t));
     configASSERT(xRecvQueue);
@@ -19,6 +39,8 @@ void shell_execute() {
     // end of where i will probably register commands here
 
     for (;;) {
-        
+        char ch = 0;
+        xQueueReceive(xRecvQueue, &ch, portMAX_DELAY);
+        printf("received %c\r\n", ch);
     }
 }
--- src/shell.h
+++ src/shell.h
@@ -7,9 +7,19 @@
 #include "task.h"
 #include "queue.h"
 
+#include "hardware/uart.h"
+#include "hardware/irq.h"
+#include "hardware/gpio.h"
+
+// uart peripheral to use
+#define UART_PERI uart0
+#define UART_IRQ UART0_IRQ
+#define UART_BAUDRATE 115200
 
 #define SHELL_INPUT_BUF_LEN 64
 #define SHELL_RECV_QUEUE_LEN 32
+
+void uart_rx_handler();
 
 void shell_init();
 void shell_execute();
