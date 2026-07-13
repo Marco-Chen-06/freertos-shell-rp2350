c31dc4e71936cdcf9db7ea023f92cab7d6070be6
Marco-Chen-06 <marcochen.work@gmail.com>
2026-07-13 19:53:01 -0400

    implement string parsing

--- src/shell.h
+++ src/shell.h
@@ -2,10 +2,12 @@
 #define SHELL_H
 
 #include <stdio.h>
+#include <string.h>
 
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
+#include "FreeRTOS_CLI.h"
 
 #include "hardware/uart.h"
 #include "hardware/irq.h"
@@ -16,8 +18,8 @@
 #define UART_IRQ UART0_IRQ
 #define UART_BAUDRATE 115200
 
-#define SHELL_INPUT_BUF_LEN 64
-#define SHELL_RECV_QUEUE_LEN 32
+#define SHELL_INPUT_BUF_LEN 64 // max input length
+#define SHELL_RECV_QUEUE_LEN 128 // max output length
 
 void uart_rx_handler();
 
