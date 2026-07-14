e10d1b37bd3e24e485f1146589044c159cdcd46a
Marco-Chen-06 <marcochen.work@gmail.com>
2026-07-14 16:14:11 -0400

    remove echo, add ps

--- src/commands.c
+++ src/commands.c
@@ -1,21 +1,23 @@
 #include "commands.h"
 
 // function prototypes for commands
-static BaseType_t prvCmdFrecho(char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString);
+static BaseType_t prvCmdPs(char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString);
 
-const CLI_Command_Definition_t xCmdFrecho = {
-    "frecho",
-    "frecho: Echoes fred\r\n",
-    prvCmdFrecho,
+const CLI_Command_Definition_t xCmdPs = {
+    "ps",
+    "ps: prints formatted data of all tasks \r\n",
+    prvCmdPs,
     0
 };
 
-static BaseType_t prvCmdFrecho(char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString) {
-    snprintf(pcWriteBuffer, xWriteBufferLen, "Fred\r\n");
+static BaseType_t prvCmdPs(char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString) {
+    const char *header = "Name          State   Priority  Stack  Num\r\n";
+    strcpy(pcWriteBuffer, header);
+    vTaskList(pcWriteBuffer + strlen(header));
     // return pdFalse to indicate completed execution
     return pdFALSE;
 }
 
 void register_commands() {
-    FreeRTOS_CLIRegisterCommand(&xCmdFrecho);
+    FreeRTOS_CLIRegisterCommand(&xCmdPs);
 }
--- src/commands.h
+++ src/commands.h
@@ -8,8 +8,6 @@
 #include "task.h"
 #include "queue.h"
 #include "FreeRTOS_CLI.h"
- 
-extern const CLI_Command_Definition_t xCmdFrecho;
 
 void register_commands(void);
 #endif
