1e093753feb9f8a5d779653d0f8ae9762235ae76
Marco-Chen-06 <marcochen.work@gmail.com>
2026-07-22 20:27:55 -0400

    add version command

--- src/commands.c
+++ src/commands.c
@@ -2,11 +2,19 @@
 
 // function prototypes for commands
 static BaseType_t prvCmdPs(char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString);
+static BaseType_t prvCmdVersion(char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString);
 
 const CLI_Command_Definition_t xCmdPs = {
     "ps",
-    "\r\nps:\r\n Prints formatted data of all tasks\r\n\r\n",
+    "ps:\r\n Prints formatted data of all tasks\r\n\r\n",
     prvCmdPs,
+    0
+};
+
+const CLI_Command_Definition_t xCmdVersion = {
+    "version",
+    "version:\r\n Prints version of project (not very useful rn)\r\n\r\n",
+    prvCmdVersion,
     0
 };
 
@@ -18,6 +26,14 @@ static BaseType_t prvCmdPs(char *pcWriteBuffer, size_t xWriteBufferLen, const ch
     return pdFALSE;
 }
 
+static BaseType_t prvCmdVersion(char *pcWriteBuffer, size_t xWriteBufferLen, const char *pcCommandString) {
+    const char *versionString = "v1.0.0\r\n";
+    strcpy(pcWriteBuffer, versionString);
+    // return pdFalse to indicate completed execution
+    return pdFALSE;
+}
+
 void register_commands() {
     FreeRTOS_CLIRegisterCommand(&xCmdPs);
+    FreeRTOS_CLIRegisterCommand(&xCmdVersion);
 }
